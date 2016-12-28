b187e0181d058ed278dfb2a2733d0146a1eb472d
Neil Roza <rubicks@users.noreply.github.com>
2016-12-28 21:41:55 +0000

    edit main.go, fix fahrenheit misspelling

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func WeatherHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEv
 		description = weather.Weather[0].Description
 	}
 	bot.Reply(evt,
-		fmt.Sprintf("The current temperature for %s is %.0f degrees farenheight (%s)",
+		fmt.Sprintf("The current temperature for %s is %.0f degrees fahrenheit (%s)",
 			weather.Name,
 			weather.Main.Temp,
 			description),
